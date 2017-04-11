9e5a578517a56818fa28ff9e1ee29288abe403d1
dabichosilva <damanetor@gmail.com>
2017-04-10 22:28:31 -0500

    fixing one and breaking another

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
@@ -115,7 +115,7 @@ public class OwnerControllerTests {
     @Test
     public void testInitUpdateOwnerForm() throws Exception {
         mockMvc.perform(get("/owners/{ownerId}/edit", TEST_OWNER_ID))
-            .andExpect(status().isOk())
+            .andExpect(!status().isOk())
             .andExpect(model().attributeExists("owner"))
             .andExpect(model().attribute("owner", hasProperty("lastName", is("Franklin"))))
             .andExpect(model().attribute("owner", hasProperty("firstName", is("George"))))
